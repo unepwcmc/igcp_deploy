57780213eaeb90a16f568ebe4349de4ce7735490
nym1k <ross.king@unep-wcmc.org>
2019-12-19 17:13:14 +0000

    Updated social share template part to use updated icons

---
+++ content/themes/igcp/template-parts/icons/icon-addthis-share.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44.615 44.615">
+  <g data-name="Ic_addthis bw" transform="translate(1 1)">
+    <circle cx="21.307" cy="21.307" r="21.307" fill="none" stroke="#383838" stroke-miterlimit="10" stroke-width="2"/>
+    <path d="M20.843 10.677a2.416 2.416 0 012.367 2.228v5.71h5.71a2.2 2.2 0 012.228 2.228 2.2 2.2 0 01-2.228 2.228h-5.71v5.706a2.228 2.228 0 11-4.456 0v-5.566h-5.849a2.321 2.321 0 01-2.228-2.368 2.2 2.2 0 012.228-2.228h5.71v-5.71a2.2 2.2 0 012.228-2.228z" fill="#383838"/>
+  </g>
+</svg>
--- content/themes/igcp/template-parts/social/social-share.php
+++ content/themes/igcp/template-parts/social/social-share.php
@@ -1,23 +1,25 @@
 <?php
   $page_url = get_site_url() . $_SERVER['REQUEST_URI'];
-  $socials = array('facebook', 'twitter', 'instagram', 'linkedin', 'youtube');
+  $socials = array('email', 'facebook', 'twitter', 'linkedin', 'addthis');
   $social_share_urls = array(
     'facebook' => 'https://www.facebook.com/sharer/sharer.php?u=' . $page_url,
     'twitter' => 'http://twitter.com/share?text=' . get_the_title() . '&url=' . $page_url,
+    'linkedin' => 'https://www.linkedin.com/sharing/share-offsite/?url=' . $page_url
   )
 ?>
 
-
-<p>Share:</p>
-<ul class="soc-Social_Icons">
-  <?php foreach ($socials as $social) :
-    $social_url = $social_share_urls[$social];
-    if ($social_url !== '') : ?>
+<div class="soc-Social">
+  <p class="soc-Social_Heading">Share:</p>
+  <ul class="soc-Social_Icons">
+    <?php foreach ($socials as $social) :
+      $social_url = $social_share_urls[$social];
+      if ($social_url != '') : ?>
       <li class="soc-Social_Icon">
         <a href="<?php echo $social_url; ?>" target="_blank" rel="noreferrer noopener">
-          <?php get_template_part( 'template-parts/icons/icon', $social ); ?>
+          <?php get_template_part( 'template-parts/icons/icon', $social . '-share'); ?>
         </a>
       </li>
     <?php endif; ?>
   <?php endforeach; ?>
 </ul>
+</div>
