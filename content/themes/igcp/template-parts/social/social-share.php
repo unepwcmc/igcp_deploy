3580624d04f32eb0820e4b40003dd5d167ba75ca
nym1k <ross.king@unep-wcmc.org>
2019-12-19 16:17:17 +0000

    Updated social share template part links to share to social platforms, removed margin from picture links UL

--- content/themes/igcp/template-parts/social/social-share.php
+++ content/themes/igcp/template-parts/social/social-share.php
@@ -1,9 +1,17 @@
-<?php $socials = array('facebook', 'twitter', 'instagram', 'linkedin', 'youtube'); ?>
+<?php
+  $page_url = get_site_url() . $_SERVER['REQUEST_URI'];
+  $socials = array('facebook', 'twitter', 'instagram', 'linkedin', 'youtube');
+  $social_share_urls = array(
+    'facebook' => 'https://www.facebook.com/sharer/sharer.php?u=' . $page_url,
+    'twitter' => 'http://twitter.com/share?text=' . get_the_title() . '&url=' . $page_url,
+  )
+?>
+
 
 <p>Share:</p>
 <ul class="soc-Social_Icons">
   <?php foreach ($socials as $social) :
-    $social_url = get_theme_mod( $social . '_url', false );
+    $social_url = $social_share_urls[$social];
     if ($social_url !== '') : ?>
       <li class="soc-Social_Icon">
         <a href="<?php echo $social_url; ?>" target="_blank" rel="noreferrer noopener">
