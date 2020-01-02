f394f70cdf8cbbf5676514b7efa83f5ecf592372
nym1k <ross.king@unep-wcmc.org>
2020-01-02 13:38:24 +0000

    Added hero to default page template, fixed footer item order on desktop

--- content/themes/igcp/page.php
+++ content/themes/igcp/page.php
@@ -12,6 +12,28 @@
  */
 
 get_header(); ?>
+
+<?php
+	// Page Hero
+
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+
+			/* Variables */
+			$hero_title = get_field( "hero_title" ) != '' ? get_field( "hero_title" ) : get_the_title();
+			set_query_var('hero-title', $hero_title);
+			set_query_var('hero-text', get_field( "hero_text" ));
+			set_query_var('hero-link-text', get_field( "button_text" ));
+			set_query_var('hero-link-url', get_field( "button_url" ));
+			set_query_var('hero-background-image', get_post_thumbnail_id(get_the_id()));
+
+			get_template_part( 'template-parts/components/heroes/hero', 'page' );
+
+		endwhile; // End of the loop.
+	endif;
+?>
+
 <div class="lyt-Container lyt-Container-hasSidebar">
 	<div class="lyt-Container_Inner">
 		<section class="lyt-Primary">
