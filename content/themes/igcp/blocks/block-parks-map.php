d0b89bb947b61f098cc07e9923355473ea868532
nym1k <ross.king@unep-wcmc.org>
2020-01-07 16:45:58 +0000

    Parks map now auto populates from park taxonomy, text added through block settings

--- content/themes/igcp/blocks/block-parks-map.php
+++ content/themes/igcp/blocks/block-parks-map.php
@@ -6,58 +6,49 @@
   */
 
   /* Variables */
+  $title = block_field( 'title', false );
   $image = block_field( 'image', false );
   $image_url = wp_get_attachment_image_src( $image, 'full-size' )[0];
+  $text = block_field( 'text', false );
+
+  $park_terms = get_terms( array(
+    'taxonomy' => 'park'
+  ));
 ?>
 
 <div class="blk-Map">
-  <div class="blk-Map_Columns">
-    <div class="blk-Map_Column blk-Map_Column-image">
-      <div class="blk-Map_ImageWrap">
-        <img src="<?php echo $image_url; ?>" alt="#" class="blk-Map_Image">
+  <div class="blk-Header">
+    <h3 class="blk-Header_Title"><?php echo $title; ?></h3>
+  </div>
+  <div class="blk-Body">
+    <div class="blk-Map_Columns">
+      <div class="blk-Map_Column blk-Map_Column-image">
+        <div class="blk-Map_ImageWrap">
+          <img src="<?php echo $image_url; ?>" alt="<?php echo $title; ?>" class="blk-Map_Image">
+        </div>
       </div>
-    </div>
-    <div class="blk-Map_Column blk-Map_Column-text">
-      <div class="blk-Map_Content">
-        <p class="blk-Map_Text">We have operations across Rwanda, DRC and Uganda, we use our resources to secure a future the mountain gorillas that call these areas home, click below to find out more about our individual gorilla families:</p>
-        <ul class="blk-Map_Items">
+      <div class="blk-Map_Column blk-Map_Column-text">
+        <div class="blk-Map_Content">
+          <p class="blk-Map_Text"><?php echo $text; ?></p>
+          <ul class="blk-Map_Items">
+            <?php foreach ($park_terms as $park_term) : ?>
 
-          <li class="blk-Map_Item">
-            <a href="" class="blk-Map_Link">
-              Virunga National Park
-              <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
-            </a>
-          </li>
+                <li class="blk-Map_Item">
+                  <a href="/park/<?php echo $park_term->slug; ?>/" class="blk-Map_Link">
+                    <?php echo $park_term->name; ?>
+                    <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
+                  </a>
+                </li>
 
-          <li class="blk-Map_Item">
-            <a href="" class="blk-Map_Link">
-              Volcanoes National Park
-              <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
-            </a>
-          </li>
-
-          <li class="blk-Map_Item">
-            <a href="" class="blk-Map_Link">
-              Mgahinga Gorilla National Park
-              <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
-            </a>
-          </li>
-
-          <li class="blk-Map_Item">
-            <a href="" class="blk-Map_Link">
-              Bwindi Impenetrable National Park
-              <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
-            </a>
-          </li>
-
-          <li class="blk-Map_Item">
-            <a href="" class="blk-Map_Link">
-              See all gorilla families
-              <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
-            </a>
-          </li>
-
-        </ul>
+            <?php endforeach; ?>
+            <li class="blk-Map_Item">
+              <a href="/families" class="blk-Map_Link">
+                See all gorilla families
+                <?php get_template_part( 'template-parts/icons/icon', 'angle-right' );?>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
