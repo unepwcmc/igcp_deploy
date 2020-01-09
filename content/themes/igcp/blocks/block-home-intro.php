55fbdb36fabc1479748c7fcd8be3e051e839513e
nym1k <ross.king@unep-wcmc.org>
2020-01-09 10:13:56 +0000

    Updated top level intro namespace in classname, created home page intro block

---
+++ content/themes/igcp/blocks/block-home-intro.php
@@ -0,0 +1,40 @@
+<?php
+  /*
+    Homepage Intro Block
+    Created by UNEP-WCMC
+    With Block Lab for Gutenberg - https://getblocklab.com/
+  */
+
+  /* Variables */
+  $lead_text = block_field( 'lead-text', false );
+  $text = block_field( 'text', false );
+  $link_url = block_field( 'link-url', false );
+  $link_text = block_field( 'link-text', false );
+  $image = block_field( 'image', false );
+  $image_url = wp_get_attachment_image_src( $image, 'full-size' )[0];
+  $image_alt = get_post_meta($image, '_wp_attachment_image_alt', TRUE);
+
+  $flipped = block_field( 'flipped', false);
+?>
+
+<div class="blk-HomeIntro">
+  <div class="blk-HomeIntro_Body">
+    <div class="blk-HomeIntro_Columns<?php if($flipped) echo ' blk-HomeIntro_Columns-flipped' ?>">
+      <div class="blk-HomeIntro_Column">
+        <div class="blk-HomeIntro_Content">
+          <p class="blk-HomeIntro_LeadText"><?php echo $lead_text; ?></p>
+          <p class="blk-HomeIntro_Text"><?php echo $text; ?></p>
+          <a class="blk-HomeIntro_Link" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
+        </div>
+      </div>
+      <div class="blk-HomeIntro_Column">
+        <div class="blk-HomeIntro_ImageWrap blk-HomeIntro_ImageWrap-primary">
+          <img class="blk-HomeIntro_Image" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
+        </div>
+        <div class="blk-HomeIntro_ImageWrap blk-HomeIntro_ImageWrap-secondary">
+          <img class="blk-HomeIntro_Image" src="<?php echo get_stylesheet_directory_uri() . '/inc/img/pattern-bg-bright.png'; ?>" alt="Background pattern">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- content/themes/igcp/blocks/block-top-level-landing-intro.php
+++ content/themes/igcp/blocks/block-top-level-landing-intro.php
@@ -19,29 +19,29 @@
   $flipped = block_field( 'flipped', false);
 ?>
 
-<div class="tlv-Intro">
-  <div class="tlv-Intro_Body">
-    <div class="tlv-Intro_Columns<?php if($flipped) echo ' tlv-Intro_Columns-flipped' ?>">
-      <div class="tlv-Intro_Column">
-        <div class="tlv-Intro_Content">
-          <p class="tlv-Intro_LeadText"><?php echo $lead_text; ?></p>
-          <p class="tlv-Intro_Text"><?php echo $text; ?></p>
-          <a class="tlv-Intro_Link" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
+<div class="blk-Intro">
+  <div class="blk-Intro_Body">
+    <div class="blk-Intro_Columns<?php if($flipped) echo ' blk-Intro_Columns-flipped' ?>">
+      <div class="blk-Intro_Column">
+        <div class="blk-Intro_Content">
+          <p class="blk-Intro_LeadText"><?php echo $lead_text; ?></p>
+          <p class="blk-Intro_Text"><?php echo $text; ?></p>
+          <a class="blk-Intro_Link" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
         </div>
       </div>
-      <div class="tlv-Intro_Column">
-        <div class="tlv-Intro_ImageWrap tlv-Intro_ImageWrap-primary">
-          <img class="tlv-Intro_Image" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
+      <div class="blk-Intro_Column">
+        <div class="blk-Intro_ImageWrap blk-Intro_ImageWrap-primary">
+          <img class="blk-Intro_Image" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
         </div>
-        <div class="tlv-Intro_ImageWrap tlv-Intro_ImageWrap-secondary">
-          <img class="tlv-Intro_Image" src="<?php echo get_stylesheet_directory_uri() . '/inc/img/pattern-bg-bright.png'; ?>" alt="Background pattern">
+        <div class="blk-Intro_ImageWrap blk-Intro_ImageWrap-secondary">
+          <img class="blk-Intro_Image" src="<?php echo get_stylesheet_directory_uri() . '/inc/img/pattern-bg-bright.png'; ?>" alt="Background pattern">
         </div>
       </div>
     </div>
   </div>
   <?php if ($show_social): ?>
-    <div class="tlv-Intro_Footer">
+    <div class="blk-Intro_Footer">
       <?php get_template_part( 'template-parts/social/social', 'share' ); ?>
     </div>
-  <?php endif; ?> 
+  <?php endif; ?>
 </div>
