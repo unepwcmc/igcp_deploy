8a00e4b353c752e4fa7ea03550b438cdd44367b3
nym1k <ross.king@unep-wcmc.org>
2020-01-15 13:16:02 +0000

    Added caption to custom image blocks, figcaption styles, updated ignore restrained width mixin

--- content/themes/igcp/blocks/block-full-width-image.php
+++ content/themes/igcp/blocks/block-full-width-image.php
@@ -9,10 +9,15 @@
   $image = block_field( 'image', false );
   $image_url = wp_get_attachment_image_src( $image, 'full-size' )[0];
   $image_alt = get_post_meta($image, '_wp_attachment_image_alt', TRUE);
+  $caption = block_field( 'caption', false );
 ?>
 
-<div class="blk-FullWidthImage">
+<figure class="blk-FullWidthImage">
   <div class="blk-FullWidthImage_ImageWrap">
     <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" class="blk-FullWidthImage_Image">
   </div>
-</div>
+  <?php if ($caption): ?>
+    <figcaption class="blk-FullWidthImage_Caption"><?php echo $caption; ?></figcaption>
+    <div class="blk-FullWidthImage_Overlay"></div>
+  <?php endif; ?>
+</figure>
--- content/themes/igcp/blocks/block-two-images.php
+++ content/themes/igcp/blocks/block-two-images.php
@@ -9,23 +9,31 @@
   $image_1 = block_field( 'image-1', false );
   $image_1_url = wp_get_attachment_image_src( $image_1, 'full-size' )[0];
   $image_1_alt = get_post_meta($image_1, '_wp_attachment_image_alt', TRUE);
+  $caption_1 = block_field( 'caption-1', false );
 
   $image_2 = block_field( 'image-2', false );
   $image_2_url = wp_get_attachment_image_src( $image_2, 'full-size' )[0];
   $image_2_alt = get_post_meta($image_2, '_wp_attachment_image_alt', TRUE);
+  $caption_2 = block_field( 'caption-2', false );
 ?>
 
 <div class="blk-TwoImages">
   <div class="blk-TwoImages_Items">
     <div class="blk-TwoImages_Item">
-      <div class="blk-TwoImages_ImageWrap">
-        <img src="<?php echo $image_1_url; ?>" alt="<?php echo $image_1_alt; ?>" class="blk-TwoImages_Image">
-      </div>
+      <figure class="blk-TwoImages_Figure">
+        <div class="blk-TwoImages_ImageWrap">
+          <img src="<?php echo $image_1_url; ?>" alt="<?php echo $image_1_alt; ?>" class="blk-TwoImages_Image">
+        </div>
+        <figcaption class="blk-TwoImages_Caption"><?php echo $caption_1; ?></figcaption>
+      </figure>
     </div>
     <div class="blk-TwoImages_Item">
-      <div class="blk-TwoImages_ImageWrap">
-        <img src="<?php echo $image_2_url; ?>" alt="<?php echo $image_2_alt; ?>" class="blk-TwoImages_Image">
-      </div>
+      <figure class="blk-TwoImages_Figure">
+        <div class="blk-TwoImages_ImageWrap">
+          <img src="<?php echo $image_2_url; ?>" alt="<?php echo $image_2_alt; ?>" class="blk-TwoImages_Image">
+        </div>
+        <figcaption class="blk-TwoImages_Caption"><?php echo $caption_2; ?></figcaption>
+      </figure>
     </div>
   </div>
 </div>
