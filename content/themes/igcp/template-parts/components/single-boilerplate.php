e12bff17a9e3bed5a4b87120feb2a46b395dbba8
nym1k <ross.king@unep-wcmc.org>
2020-01-15 14:31:52 +0000

    Added background colour to body, boilerplate on single template now conditionally shows, header styles for entry items

--- content/themes/igcp/template-parts/components/single-boilerplate.php
+++ content/themes/igcp/template-parts/components/single-boilerplate.php
@@ -1,11 +1,12 @@
-<div class="art-Boilerplate">
-  <div class="art-Boilerplate_Text">
-      <?php echo get_theme_mod( 'blog_posts_boilerplate' ); ?>
+<?php
+  /* Variables */
+  $text = get_theme_mod( 'blog_posts_boilerplate' );
+?>
+
+<?php if ($text): ?>
+  <div class="art-Boilerplate">
+    <div class="art-Boilerplate_Text">
+      <?php echo $text; ?>
+    </div>
   </div>
-</div>
-
-<?php /*
-
-The International Gorilla Conservation Programme (IGCP) currently consists of Conservation International, Fauna & Flora International (FFI) and the World Wide Fund for Nature (WWF). We recognise that the earth's survival is dependent on humanity's ability to maintain a healthy and balanced environment that includes all species of wildlife.
-
-*/ ?>
+<?php endif; ?>
