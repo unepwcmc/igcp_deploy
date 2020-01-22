cdedd9f62e456b9eb54558a0d5abf0405f2d080c
nym1k <ross.king@unep-wcmc.org>
2020-01-22 13:53:39 +0000

    Sidebar padding, search bar positioning, parks map svg positioning

--- content/themes/igcp/sidebar.php
+++ content/themes/igcp/sidebar.php
@@ -7,19 +7,30 @@
  */
 ?>
 <aside class="lyt-Sidebar">
-	<?php get_template_part( 'template-parts/filters/toggle', 'posts'); ?>
-	<?php get_template_part( 'template-parts/filters/filters', 'posts'); ?>
-	<?php get_template_part( 'template-parts/filters/toggle', 'families'); ?>
-	<?php get_template_part( 'template-parts/filters/filters', 'families'); ?>
-	<?php get_template_part( 'template-parts/filters/toggle', 'team'); ?>
-	<?php get_template_part( 'template-parts/filters/filters', 'team'); ?>
-	<?php get_template_part( 'template-parts/filters/toggle', 'library'); ?>
-	<?php get_template_part( 'template-parts/filters/filters', 'library'); ?>
-	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-	  <div class="sbr-Widgets">
-	    <ul class="sbr-Widgets_Items">
-					<?php dynamic_sidebar( 'sidebar' ); ?>
-	    </ul>
-	  </div>
-	<?php	endif; ?>
+	<div class="sbr-Sidebar"><?php
+		// Post archive page
+		get_template_part( 'template-parts/filters/toggle', 'posts');
+		get_template_part( 'template-parts/filters/filters', 'posts');
+
+		// Family archive page
+		get_template_part( 'template-parts/filters/toggle', 'families');
+		get_template_part( 'template-parts/filters/filters', 'families');
+
+		// Team archive page
+		get_template_part( 'template-parts/filters/toggle', 'team');
+		get_template_part( 'template-parts/filters/filters', 'team');
+
+		// Library archive page
+		get_template_part( 'template-parts/filters/toggle', 'library');
+		get_template_part( 'template-parts/filters/filters', 'library');
+
+		// Widgets
+		if ( is_active_sidebar( 'sidebar' ) ) : ?>
+		  <div class="sbr-Widgets">
+		    <ul class="sbr-Widgets_Items">
+						<?php dynamic_sidebar( 'sidebar' ); ?>
+		    </ul>
+		  </div>
+		<?php	endif;
+	?></div>
 </aside>
