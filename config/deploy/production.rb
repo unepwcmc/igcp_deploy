250bcd4a23e5bf124f3ec6430c92dab07170fa49
Stuart Watson <stuart.watson@unep-wcmc.org>
2020-01-27 13:31:22 +0000

    Added production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,19 +3,20 @@
 ################################################################################
 
 # The Git branch this environment should be attached to.
-set :branch, 'master'
+set :branch, 'production'
 
 # The environment's name. To be used in commands and other references.
 set :stage, :production
 
 # The URL of the website in this environment.
-set :stage_url, 'http://www.example.com'
+set :stage_url, 'igcp.production.wordpress-linode.linode.unep-wcmc.org'
 
 # The environment's server credentials
-server 'XXX.XXX.XX.XXX', user: 'SSHUSER', roles: %w(web app db)
+server 'production.wordpress-linode.linode.unep-wcmc.org', user: 'wcmc', roles: %w(web app db)
 
 # The deploy path to the website on this environment's server.
-set :deploy_to, '/deploy/to/path'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
 
 # The web user on this environment's server.
 set :web_user, 'www-data'
+
