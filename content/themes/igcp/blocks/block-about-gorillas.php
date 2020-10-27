92355e1f267201dcdb021434489d4ddb8761d435
nym1k <ross.king@unep-wcmc.org>
2020-10-27 13:37:10 +0000

    Removed images from about gorillas block

--- content/themes/igcp/blocks/block-about-gorillas.php
+++ content/themes/igcp/blocks/block-about-gorillas.php
@@ -10,10 +10,6 @@
 
   $title = block_field( 'title', false );
   $text = block_field( 'text', false );
-  $image_1 = block_field( 'image-1', false );
-  $image_1_src = wp_get_attachment_image_src( $image_1, 'full-size' )[0];
-  $image_2 = block_field( 'image-2', false );
-  $image_2_src = wp_get_attachment_image_src( $image_2, 'full-size' )[0];
   $block_quote = block_field( 'block-quote', false );
 ?>
 
@@ -21,18 +17,6 @@
   <?php get_template_part( 'template-parts/components/family-stats' ); ?>
   <h3 class="abt-Gorillas_Title"><?php echo $title; ?></h3>
   <p class="abt-Gorillas_Text"><?php echo $text; ?></p>
-  <div class="abt-Gorillas_Images <?php if ($image_1 != '' && $image_2 != '') echo ' abt-Gorillas_Images-flex'; ?>">
-    <div class="abt-Gorillas_Image">
-      <div class="abt-Gorillas_ImageWrap">
-        <img src="<?php echo $image_1_src; ?>" alt="<?php echo $title; ?>">
-      </div>
-    </div>
-    <div class="abt-Gorillas_Image">
-      <div class="abt-Gorillas_ImageWrap">
-        <img src="<?php echo $image_2_src; ?>" alt="<?php echo $title; ?>">
-      </div>
-    </div>
-  </div>
   <blockquote class="abt-Gorillas_Quote">
     <p class="abt-Gorillas_QuoteText"><?php echo $block_quote; ?></p>
   </blockquote>
